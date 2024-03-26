4fed1cbd993ebc297ab34c655e16d6987c79175b
Ranit Dey <ranitdey@fb.com>
2024-03-26 22:25:34 +0000

    first commit

---
+++ app/components/Courses/index.tsx
@@ -0,0 +1,193 @@
+"use client";
+import Slider from "react-slick";
+import React, { Component } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { StarIcon } from "@heroicons/react/24/solid";
+
+// CAROUSEL DATA
+
+interface DataType {
+  heading: string;
+  heading2: string;
+  imgSrc: string;
+  name: string;
+  students: number;
+  classes: number;
+  price: number;
+  rating: number;
+}
+
+const postData: DataType[] = [
+  {
+    heading: "Google",
+    heading2: "",
+    name: "",
+    imgSrc: "/assets/courses/google_.png",
+    students: 30,
+    classes: 5,
+    price: 800,
+    rating: 4.7,
+  },
+  {
+    heading: "Facebook",
+    heading2: "",
+    name: "",
+    imgSrc: "/assets/courses/netflix_.png",
+    students: 30,
+    classes: 5,
+    price: 800,
+    rating: 4.7,
+  },
+  {
+    heading: "Apple",
+    heading2: "",
+    name: "",
+    imgSrc: "/assets/courses/microsoft_.png",
+    students: 30,
+    classes: 5,
+    price: 800,
+    rating: 4.7,
+  },
+];
+
+// CAROUSEL SETTINGS
+
+export default class MultipleItems extends Component {
+  render() {
+    const settings = {
+      dots: false,
+      infinite: true,
+      slidesToShow: 3,
+      // centerMode: true,
+      slidesToScroll: 2,
+      arrows: false,
+      autoplay: false,
+      speed: 500,
+      cssEase: "linear",
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+          },
+        },
+      ],
+    };
+
+    return (
+      <div id="courses">
+        <div className="mx-auto max-w-7xl sm:py-8 px-4 lg:px-8 ">
+          <div className="sm:flex justify-between items-center">
+            <h3 className="text-midnightblue text-4xl lg:text-55xl font-semibold mb-5 sm:mb-0">
+              Dedicated coaching.
+            </h3>
+            <Link
+              href={"/"}
+              className="text-Blueviolet text-lg font-medium space-links"
+            >
+              Explore more&nbsp;&gt;&nbsp;
+            </Link>
+          </div>
+
+          <Slider {...settings}>
+            {postData.map((items, i) => (
+              <div key={i}>
+                <div className="bg-white m-3 px-3 pt-3 pb-12 my-20 shadow-courses rounded-2xl">
+                  <div className="relative rounded-3xl">
+                    <Image
+                      src={items.imgSrc}
+                      alt="gaby"
+                      width={389}
+                      height={262}
+                      className="object-contain h-48 w-full"
+                    />
+                    {/* <div className="absolute right-5 -bottom-2 bg-ultramarine rounded-full p-6">
+                      <h3 className="text-white uppercase text-center text-sm font-medium">
+                        best <br /> seller
+                      </h3>
+                    </div> */}
+                  </div>
+
+                  <div className="px-3">
+                    <h4 className="text-2xl font-bold pt-6 text-black">
+                      {items.heading}
+                    </h4>
+                    <h4 className="text-2xl font-bold pt-1 text-black">
+                      {items.heading2}
+                    </h4>
+
+                    <div>
+                      <h3 className="text-base font-normal pt-6 opacity-75">
+                        {items.name}
+                      </h3>
+                    </div>
+
+                    <div className="flex justify-between items-center py-6">
+                      <div className="flex gap-4">
+                        <h3 className="text-red text-22xl font-medium">
+                          {items.rating}
+                        </h3>
+                        <div className="flex">
+                          <StarIcon className="h-5 w-5 text-gold" />
+                          <StarIcon className="h-5 w-5 text-gold" />
+                          <StarIcon className="h-5 w-5 text-gold" />
+                          <StarIcon className="h-5 w-5 text-gold" />
+                          <StarIcon className="h-5 w-5 text-gold" />
+                        </div>
+                      </div>
+                      <div>
+                        <h3 className="text-3xl font-medium">${items.price}</h3>
+                      </div>
+                    </div>
+
+                    <hr style={{ color: "#C4C4C4" }} />
+
+                    <div className="flex justify-between pt-6">
+                      <div className="flex gap-4">
+                        <Image
+                          src={"/assets/courses/book-open.svg"}
+                          alt="users"
+                          width={24}
+                          height={24}
+                          className="inline-block m-auto"
+                        />
+                        <h3 className="text-base font-medium text-black opacity-75">
+                          {items.classes} classes
+                        </h3>
+                      </div>
+                      <div className="flex gap-4">
+                        <Image
+                          src={"/assets/courses/users.svg"}
+                          alt="users"
+                          width={24}
+                          height={24}
+                          className="inline-block m-auto"
+                        />
+                        <h3 className="text-base font-medium text-black opacity-75">
+                          {items.students} hours
+                        </h3>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ app/components/Mentor/index.tsx
@@ -0,0 +1,282 @@
+"use client";
+import Slider from "react-slick";
+import React, { Component } from "react";
+import Image from "next/image";
+
+// CAROUSEL DATA
+
+interface DataType {
+  profession: string;
+  name: string;
+  imgSrc: string;
+}
+
+const postData: DataType[] = [
+  {
+    profession: "Software Engineer at Meta",
+    name: "Ranit Dey",
+    imgSrc: "/assets/linkedin/ranit.png",
+  },
+  {
+    profession: "Software Engineer at Delivery Hero",
+    name: "Satyam Kumar",
+    imgSrc: "/assets/linkedin/satyam.png",
+  },
+  {
+    profession: "Testing engineer at Grab",
+    name: "Yusuf Ansari",
+    imgSrc: "/assets/linkedin/tusuf.png",
+  },
+  {
+    profession: "Testing engineer at Phonepe",
+    name: "Saket Singh",
+    imgSrc: "/assets/linkedin/saket.png",
+  },
+];
+
+// CAROUSEL SETTINGS
+
+function SampleNextArrow(props: { className: any; style: any; onClick: any }) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "flex",
+        justifyContent: "center",
+        position: "absolute",
+        alignItems: "center",
+        background: "#D5EFFA",
+        padding: "28px",
+        borderRadius: "30px",
+        border: "1px solid #1A21BC",
+      }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props: { className: any; style: any; onClick: any }) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#D5EFFA",
+        padding: "28px",
+        borderRadius: "30px",
+        border: "1px solid #1A21BC",
+      }}
+      onClick={onClick}
+    />
+  );
+}
+
+export default class MultipleItems extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      slidesToShow: 3,
+      // centerMode: true,
+      slidesToScroll: 2,
+      arrows: false,
+      autoplay: false,
+      speed: 4000,
+      nextArrow: (
+        <SampleNextArrow
+          className={undefined}
+          style={undefined}
+          onClick={undefined}
+        />
+      ),
+      prevArrow: (
+        <SamplePrevArrow
+          className={undefined}
+          style={undefined}
+          onClick={undefined}
+        />
+      ),
+      autoplaySpeed: 4500,
+      cssEase: "linear",
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+          },
+        },
+        {
+          breakpoint: 1000,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+          },
+        },
+        {
+          breakpoint: 530,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+          },
+        },
+      ],
+    };
+
+    return (
+      <div className="py-10 sm:py-24 bg-paleblue" id="mentor">
+        <div className="mx-auto max-w-2xl lg:max-w-7xl sm:py-4 px-4 lg:px-8 relative">
+          <h2 className="lh-82 text-midnightblue text-4xl md:text-55xl text-center md:text-start font-semibold">
+            Meet with our top <br /> mentors.
+          </h2>
+
+          <Slider {...settings}>
+            {postData.map((items, i) => (
+              <div key={i}>
+                <div className="w-full h-20 bg-gray-100 px-10 pt-10">
+                  <div className="relative mt-16 mb-32 max-w-sm mx-auto mt-24">
+                    <div className="rounded overflow-hidden shadow-md bg-white">
+                      <div className="absolute -mt-20 w-full flex justify-center">
+                        <div className="h-32 w-32">
+                          <img
+                            src={items.imgSrc}
+                            className="rounded-full object-cover h-full w-full shadow-md"
+                          />
+                        </div>
+                      </div>
+                      <div className="px-6 mt-16">
+                        <h1 className="font-bold text-3xl text-center mb-1">
+                          {items.name}
+                        </h1>
+                        <p className="text-gray-800 text-sm text-center">
+                          {items.profession}
+                        </p>
+                        <p className="text-center text-gray-600 text-base pt-8 font-normal"></p>
+                        <div className="flex pb-8 gap-4">
+                          <div
+                            draggable="true"
+                            role="button"
+                            title="Hover chip"
+                            className="h-8 px-3 w-max flex gap-2 items-center rounded-full bg-gray-200 text-gray-700 hover:bg-gray-300 hover:bg-opacity-75 focus:bg-gray-300 focus:text-blue-900 active:text-primary active:bg-blue-100 disabled:bg-gray-100 disabled:text-gray-400 dark:bg-gray-700 dark:text-gray-300 dark:active:text-primary"
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="16"
+                              height="16"
+                              fill="currentColor"
+                              className="bi bi-meta"
+                              viewBox="0 0 16 16"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                d="M8.217 5.243C9.145 3.988 10.171 3 11.483 3 13.96 3 16 6.153 16.001 9.907c0 2.29-.986 3.725-2.757 3.725-1.543 0-2.395-.866-3.924-3.424l-.667-1.123-.118-.197a54.944 54.944 0 0 0-.53-.877l-1.178 2.08c-1.673 2.925-2.615 3.541-3.923 3.541C1.086 13.632 0 12.217 0 9.973 0 6.388 1.995 3 4.598 3c.319 0 .625.039.924.122.31.086.611.22.913.407.577.359 1.154.915 1.782 1.714Zm1.516 2.224c-.252-.41-.494-.787-.727-1.133L9 6.326c.845-1.305 1.543-1.954 2.372-1.954 1.723 0 3.102 2.537 3.102 5.653 0 1.188-.39 1.877-1.195 1.877-.773 0-1.142-.51-2.61-2.87l-.937-1.565ZM4.846 4.756c.725.1 1.385.634 2.34 2.001A212.13 212.13 0 0 0 5.551 9.3c-1.357 2.126-1.826 2.603-2.581 2.603-.777 0-1.24-.682-1.24-1.9 0-2.602 1.298-5.264 2.846-5.264.091 0 .181.006.27.018Z"
+                              ></path>
+                            </svg>
+                            <span className="block text-sm font-medium">
+                              Meta
+                            </span>
+                          </div>
+
+                          <div
+                            draggable="true"
+                            role="button"
+                            title="Hover chip"
+                            className="h-8 px-3 w-max flex gap-2 items-center rounded-full bg-gray-200 text-gray-700 hover:bg-gray-300 hover:bg-opacity-75 focus:bg-gray-300 focus:text-blue-900 active:text-primary active:bg-blue-100 disabled:bg-gray-100 disabled:text-gray-400 dark:bg-gray-700 dark:text-gray-300 dark:active:text-primary"
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="16"
+                              height="16"
+                              fill="currentColor"
+                              className="bi bi-apple"
+                              viewBox="0 0 16 16"
+                            >
+                              <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z"></path>
+                              <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z"></path>
+                            </svg>
+                            <span className="block text-sm font-medium">
+                              Apple
+                            </span>
+                          </div>
+                        </div>
+                        {/* <div className="w-full flex justify-center pt-5 pb-5">
+                          <a href="#" className="mx-5">
+                            <div aria-label="Github">
+                              
+                            </div>
+                          </a>
+                          <a href="#" className="mx-5">
+                            <div aria-label="Twitter">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="#718096"
+                                stroke-width="1.5"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                className="feather feather-twitter"
+                              >
+                                <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+                              </svg>
+                            </div>
+                          </a>
+                          <a href="#" className="mx-5">
+                            <div aria-label="Instagram">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="#718096"
+                                stroke-width="1.5"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                className="feather feather-instagram"
+                              >
+                                <rect
+                                  x="2"
+                                  y="2"
+                                  width="20"
+                                  height="20"
+                                  rx="5"
+                                  ry="5"
+                                ></rect>
+                                <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+                                <line
+                                  x1="17.5"
+                                  y1="6.5"
+                                  x2="17.51"
+                                  y2="6.5"
+                                ></line>
+                              </svg>
+                            </div>
+                          </a>
+                        </div> */}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    );
+  }
+}
